7914e952ed582fbf67b047ef09b79e9e45d57c5f
Stuart Terrett <shterrett@gmail.com>
2015-02-24 05:41:07 -0500

    Begins PS 6

---
+++ ps_6/rubik/solver.py
@@ -0,0 +1,11 @@
+import rubik
+
+def shortest_path(start, end):
+    """
+    Using 2-way BFS, finds the shortest path from start_position to
+    end_position. Returns a list of moves. 
+
+    You can use the rubik.quarter_twists move set.
+    Each move can be applied using rubik.perm_apply
+    """
+    raise NotImplementedError
